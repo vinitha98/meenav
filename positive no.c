7342e0215f14e71554ed6c3f225c1d7e72402025
vinitha98 <31506076+vinitha98@users.noreply.github.com>
2018-01-10 16:08:30 +0530

    Create positive no.c

---
+++ positive no.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("Enter any integer");
+scanf("%d",&n);
+if(n<=0)
+{
+if(n==0)
+{
+printf("Given number is zero");
+}
+else
+{
+printf("Given number is negative");
+}
+}
+else
+{
+printf("Give number 
